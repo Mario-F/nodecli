d79be44c4e404d79a007759ed1e9a77bf73f874b
Mario-F <github@fritschen.net>
2022-07-06 18:11:36 +0200

    feat: implemented config.get
    Signed-off-by: Mario-F <github@fritschen.net>

---
+++ src/cmds/config_cmds/get.ts
@@ -0,0 +1,16 @@
+import i18n from 'i18n'
+import stringify from 'json-stringify-safe'
+import { config } from '../../config'
+
+type ConfigGetArguments = {
+  configname: string
+}
+
+export const command = 'get <configname>'
+export const desc = i18n.__('Shows the current config including metadata')
+export const builder = {}
+export const handler = function (argv: ConfigGetArguments): void {
+  const result = config.get(argv.configname)
+  // eslint-disable-next-line no-console
+  console.log(stringify(result))
+}
