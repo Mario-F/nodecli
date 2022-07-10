941f4299b33d9de606ee13e3bd88e94335f09253
Mario-F <github@fritschen.net>
2022-07-10 15:15:52 +0200

    feat: added config usage in hello command
    Signed-off-by: Mario-F <github@fritschen.net>

--- src/cmds/hello.ts
+++ src/cmds/hello.ts
@@ -1,4 +1,5 @@
 import i18n from 'i18n'
+import { config } from '../config'
 
 export const command = 'hello'
 export const desc = 'Greet with a hello world'
@@ -6,4 +7,12 @@ export const builder = {}
 export const handler = function (): void {
   // eslint-disable-next-line no-console
   console.log(i18n.__('Hello world!'))
+  const additionalText = config.get('hello')
+  if (!additionalText) {
+    // eslint-disable-next-line no-console
+    console.log(i18n.__('No additional text configured, use command config set hello "your text here"'))
+    return
+  }
+  // eslint-disable-next-line no-console
+  console.log(additionalText)
 }
